c66d69d809af19342bb10e1308c3d7b1c32fd908
toowee <byprob03@gmail.com>
2023-09-16 09:56:03 +0500

    практика тртетья (3)

--- PZ_02/Program.cs
+++ PZ_02/Program.cs
@@ -4,7 +4,34 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            double s, t, v;
+            Console.WriteLine("Введите перменную m");
+            int m = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите перменную x");
+            double x = Convert.ToDouble(Console.ReadLine());
+            if (m < 0)
+            {
+                s = m - (m * x * 1.0) * x * 1.0 / (x + 1);
+            }
+            else
+            {
+                s = -10.6 * x;
+            }
+            if (s <= -1)
+            {
+                t = m * s + Math.Sin(x) + m;
+            }
+            else
+            {
+                t = s - Math.Pow(Math.Cos(s / (x * 1.0)), 2);
+            }
+            v = x + (3 * 1.0 * m) * s - (s * (t * 1.0));
+            Console.WriteLine("Вы ввели " + m);
+            Console.WriteLine("Вы также ввели " + x);
+            Console.WriteLine("Значение s = " + s);
+            Console.WriteLine("Значение t = " + t);
+            Console.WriteLine("Значение v = " + v);
+
         }
     }
 }
---
+++ PZ_03/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace PZ_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите номер месяца");
+            int month = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Праздники в этом месяце:");
+            switch (month)
+            {
+                case 1:
+                    Console.WriteLine("1 января - Новый год");
+                    Console.WriteLine("7 января - Рождество");
+                    break;
+                case 2:
+                    Console.WriteLine("23  февраля - День защитника Отечества");
+                    break;
+                case 3:
+                    Console.WriteLine("8 марта - Международный женский день");
+                    break;
+                case 4:
+                    Console.WriteLine("Нет государственных праздников");
+                    break;
+                case 5:
+                    Console.WriteLine("1 мая - Праздник весны и труда");
+                    Console.WriteLine("9 мая - День Победы");
+                    break;
+                case 6:
+                    Console.WriteLine("12 июня - День России");
+                    break;
+                case 7:
+                    Console.WriteLine("Нет государственных праздников");
+                    break;
+                case 8:
+                    Console.WriteLine("Нет государственных праздников");
+                    break;
+                case 9:
+                    Console.WriteLine("Нет государственных праздников");
+                    break;
+                case 10:
+                    Console.WriteLine("Нет государственных праздников");
+                    break;
+                case 11:
+                    Console.WriteLine("4 ноября - День Народного Единства");
+                    break;
+                case 12:
+                    Console.WriteLine("Нет государственных праздников");
+                    break;
+                default:
+                    Console.WriteLine("Ты ввел не то");
+                    break;
+            }
+        }
+    }
+}
