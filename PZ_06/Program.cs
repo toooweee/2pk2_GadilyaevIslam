203614adb4e977762acbc315f065535633220358
toowee <byprob03@gmail.com>
2023-09-29 17:54:11 +0500

    практика 7

--- PZ_06/Program.cs
+++ PZ_06/Program.cs
@@ -4,7 +4,40 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine();
+            Random rnd = new Random();
+            int[] arr = new int[20];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rnd.Next(20);
+                Console.WriteLine(arr[i]);
+            }
+            int temp;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] > arr[j])
+                    {
+                        temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+            }
+
+            int count = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if ((arr[i] == arr[j]))
+                    {
+                        count++;
+                    }
+                }
+            }
+            Console.WriteLine("Количество повторяющихся чисел равно: " + count);
         }
     }
 }
---
+++ PZ_07/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace PZ_07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int [,] arr = new int[10, 10];
+            for(int i  = 0; i < 10; i++)
+            {
+                for(int j = 0; j < 10; j++)
+                {
+                    arr[i, j] = rand.Next(0, 10);
+                }
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    Console.Write(arr[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Введите число");
+            int count = 0;
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    if (arr[i, j] == n)
+                    {
+                        count++;
+                    }
+                }
+            }
+            Console.WriteLine($"Число {n} повторяется  {count} раз");
+
+
+        }
+    }
+}
