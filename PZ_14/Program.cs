e94144b5da789fda9dab19175cdb5d43f3cdbe18
toowee <byprob03@gmail.com>
2023-12-03 13:57:38 +0500

    практика 14

---
+++ PZ_14/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace PZ_14
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                Console.Write("Введите целое число x (не более 100): ");
+                if (int.TryParse(Console.ReadLine(), out int x) && x > 0 && x <= 100)
+                {
+                    string filePath = "output.txt"; // Имя файла, куда будем записывать результат
+                    using (StreamWriter writer = new StreamWriter(filePath))
+                    {
+                        // Формирование строк с символами '*'
+                        for (int i = 1; i <= x; i++)
+                        {
+                            string line;
+                            if (i <= x / 2)
+                            {
+                                line = new string('*', i);
+                            }
+                            else
+                            {
+                                line = new string('*', x - i + 1);
+                            }
+
+                            // Запись строки в файл
+                            writer.WriteLine(line);
+
+                            // Вывод строки в консоль
+                            Console.WriteLine(line);
+                        }
+
+                        Console.WriteLine($"Результат записан в файл: {filePath}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка: Введите корректное целое число от 1 до 100.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Произошла ошибка: {ex.Message}");
+            }
+        }
+    }
+}
